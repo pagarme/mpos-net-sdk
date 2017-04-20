ea5a85f154b350190b09dda72ccc824cc076b5b6
Lucas Duarte <lc.sales.duarte@gmail.com>
2017-04-20 16:58:02 -0300

    PaymentTest: add configuration infrastructure
    Add a configuration infrastructure for the PaymentTest project. Prior to
    this, changes on this program due to testing purposes would be
    committed as code changes, which could be confusing for tracking the
    whole project's actual logic history.
    
    Example config files have been added for Windows and Linux. They should
    be renamed to "App.config" with a proper configuration to take effect.
    
    Create Config.cs, a wrapper around ConfigurationManager to manage the
    aforementioned configurations.
    
    Bump the PaymentTest project version.

---
+++ PaymentTest/Config.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+
+namespace PaymentTest
+{
+    public static class Config
+    {
+        static Config()
+        {
+            Port = ConfigurationManager.AppSettings["Port"];
+            BaudRate = Int32.Parse(ConfigurationManager.AppSettings["BaudRate"]);
+
+            EncryptionKey = ConfigurationManager.AppSettings["EncryptionKey"];
+            ApiKey = ConfigurationManager.AppSettings["ApiKey"];
+
+            SqlitePath = ConfigurationManager.AppSettings["SqlitePath"];
+        }
+
+        public readonly static String Port;
+        public readonly static Int32 BaudRate;
+
+        public readonly static String EncryptionKey;
+        public readonly static String ApiKey;
+
+        public readonly static String SqlitePath;
+    }
+}
--- PaymentTest/PaymentProcessor.cs
+++ PaymentTest/PaymentProcessor.cs
@@ -14,22 +14,18 @@ namespace PaymentTest
 
         public PaymentProcessor(string device)
         {
-            _port = new SerialPort(device, 140000, Parity.None, 8, StopBits.One);
+            _port = new SerialPort(device, Config.BaudRate, Parity.None, 8, StopBits.One);
             _port.Open();
 
-            _mpos = new Mpos(_port.BaseStream, "ek_test_f9cws0bU9700VqWE4UDuBlKLbvX4IO", "/tmp/");
-            //_mpos = new Mpos(_port.BaseStream, "ek_test_UT6AN4fDN3BCUgo6kxUiOq6S20dbKc");
+            _mpos = new Mpos(_port.BaseStream, Config.EncryptionKey, Config.SqlitePath);
             _mpos.NotificationReceived += (sender, e) => Console.WriteLine("Status: {0}", e);
             _mpos.TableUpdated += (sender, e) => Console.WriteLine("LOADED: {0}", e);
             _mpos.Errored += (sender, e) => Console.WriteLine("I GOT ERROR {0}", e);
             _mpos.PaymentProcessed += (sender, e) => Console.WriteLine("HEY CARD HASH " + e.CardHash);
             _mpos.FinishedTransaction += (sender, e) => Console.WriteLine("FINISHED TRANSACTION!");
 
-            //PagarMeService.DefaultApiEndpoint = "http://192.168.64.2:3000";
-            //PagarMeService.DefaultEncryptionKey = "ek_live_bspDfnKtdZahowfxSxuYTdYxaaDp1v";
-            //PagarMeService.DefaultApiKey = "ak_live_JPHX33BR4omHj3ewCEghXsh12BH8VG";
-            PagarMeService.DefaultEncryptionKey = "ek_test_f9cws0bU9700VqWE4UDuBlKLbvX4IO";
-            PagarMeService.DefaultApiKey = "ak_test_NQEfPH4ktp7c9Zb0bpi1u1XkjpFCTH";
+            PagarMeService.DefaultEncryptionKey = Config.EncryptionKey;
+            PagarMeService.DefaultApiKey = Config.ApiKey;
         }
 
         public async Task Initialize()
@@ -37,9 +33,8 @@ namespace PaymentTest
             await _mpos.Initialize();
 
             Console.WriteLine("Asking for tables to be synchronized...");
-            //await _mpos.SynchronizeTables(true);
-            //Console.WriteLine ("SynchronizeTables called.");
-            //_mpos.Display("Hello, world!");
+            await _mpos.SynchronizeTables(true);
+            Console.WriteLine("SynchronizeTables called.");
         }
 
         public async Task Pay(int amount)
