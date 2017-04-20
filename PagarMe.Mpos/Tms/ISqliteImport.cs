6c922ef60d1ee8e2495dae7518502f04d203a707
Lucas Duarte <lc.sales.duarte@gmail.com>
2017-04-20 17:40:43 -0300

    sqlite-net: handle 32bit and 64bit native libraries
    Handle both 32bit and 64bit instruction sets for sqlite3 native
    libraries on Windows and Linux-like operating systems. This is needed
    for the PagarMe.Mpos project to support both architectures.
    
    This is necessary because P/Invoke's DllImport does not allow for
    variable library path names. This means we need to create a class for
    each architecture (with different library names) and specify all the
    entrypoints. An "ISqliteImport" interface has been created to unify
    these classes.
    
    The expected names for the libraries are, respectively, "sqlite3_32" and
    "sqlite3_64".

---
+++ PagarMe.Mpos/Tms/ISqliteImport.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using static SQLite.SQLite3;
+
+namespace SQLite
+{
+    internal interface ISqliteImport
+    {
+        int Threadsafe();
+
+        Result Open([MarshalAs(UnmanagedType.LPStr)] string filename, out IntPtr db);
+
+        Result Open([MarshalAs(UnmanagedType.LPStr)] string filename, out IntPtr db, int flags, IntPtr zvfs);
+
+        Result Open(byte[] filename, out IntPtr db, int flags, IntPtr zvfs);
+
+        Result Open16([MarshalAs(UnmanagedType.LPWStr)] string filename, out IntPtr db);
+
+        Result EnableLoadExtension(IntPtr db, int onoff);
+
+        Result Close(IntPtr db);
+
+        Result Close2(IntPtr db);
+
+        Result Initialize();
+
+        Result Shutdown();
+
+        Result Config(ConfigOption option);
+
+        int SetDirectory(uint directoryType, string directoryPath);
+
+        Result BusyTimeout(IntPtr db, int milliseconds);
+
+        int Changes(IntPtr db);
+
+        Result Prepare2(IntPtr db, [MarshalAs(UnmanagedType.LPStr)] string sql, int numBytes, out IntPtr stmt, IntPtr pzTail);
+
+#if NETFX_CORE
+        Result Prepare2 (IntPtr db, byte[] queryBytes, int numBytes, out IntPtr stmt, IntPtr pzTail);
+#endif
+
+        Result Step(IntPtr stmt);
+
+        Result Reset(IntPtr stmt);
+
+        Result Finalize(IntPtr stmt);
+
+        long LastInsertRowid(IntPtr db);
+
+        IntPtr Errmsg(IntPtr db);
+
+        int BindParameterIndex(IntPtr stmt, [MarshalAs(UnmanagedType.LPStr)] string name);
+
+        int BindNull(IntPtr stmt, int index);
+
+        int BindInt(IntPtr stmt, int index, int val);
+
+        int BindInt64(IntPtr stmt, int index, long val);
+
+        int BindDouble(IntPtr stmt, int index, double val);
+
+        int BindText(IntPtr stmt, int index, [MarshalAs(UnmanagedType.LPWStr)] string val, int n, IntPtr free);
+
+        int BindBlob(IntPtr stmt, int index, byte[] val, int n, IntPtr free);
+
+        int ColumnCount(IntPtr stmt);
+
+        IntPtr ColumnName(IntPtr stmt, int index);
+
+        IntPtr ColumnName16Internal(IntPtr stmt, int index);
+
+        ColType ColumnType(IntPtr stmt, int index);
+
+        int ColumnInt(IntPtr stmt, int index);
+
+        long ColumnInt64(IntPtr stmt, int index);
+
+        double ColumnDouble(IntPtr stmt, int index);
+
+        IntPtr ColumnText(IntPtr stmt, int index);
+
+        IntPtr ColumnText16(IntPtr stmt, int index);
+
+        IntPtr ColumnBlob(IntPtr stmt, int index);
+
+        int ColumnBytes(IntPtr stmt, int index);
+
+        ExtendedResult ExtendedErrCode(IntPtr db);
+
+        int LibVersionNumber();
+
+    }
+}
