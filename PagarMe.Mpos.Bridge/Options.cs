9d5fce8a6f7bba60f9f86d75586d56c8a23ce1d6
Lucas Duarte <lc.sales.duarte@gmail.com>
2017-05-03 14:59:00 -0300

    Fix baud rate

--- PagarMe.Mpos.Bridge/Options.cs
+++ PagarMe.Mpos.Bridge/Options.cs
@@ -19,7 +19,7 @@ namespace PagarMe.Mpos.Bridge
         [Option('d', "data-path", Required = false, HelpText = "Database path", DefaultValue = "<appdata>")]
         public string DataPath { get; set; }
 
-        [Option('r', "baud-rate", Required = false, HelpText = "Baud rate for serial port", DefaultValue = 115200)]
+        [Option('r', "baud-rate", Required = false, HelpText = "Baud rate for serial port")]
         public int BaudRate { get; set; }
 
         [Option('k', "encryption-key", Required = false, HelpText = "Enterprise Encryption Key")]
