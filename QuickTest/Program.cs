48b065edd45a77bd4b63abc1827bfdb6b61b017a
Lucas Duarte <lc.sales.duarte@gmail.com>
2017-05-03 15:33:35 -0300

    Remove code from original tests of bridge

--- QuickTest/Program.cs
+++ QuickTest/Program.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Linq;
-using System.Threading.Tasks;
 using PagarMe.Mpos.Bridge;
-using PagarMe.Mpos.Bridge.Commands;
 using PagarMe.Generic;
-using PagarMe;
 using CommandLine;
 using System.Threading;
 
@@ -43,53 +39,5 @@ namespace QuickTest
             return 0;
         }
 
-        private static async Task ExecuteAsync()
-        {
-            var bridge = new MposBridge(options);
-            bridge.Start();
-            var context = bridge.GetContext(null);
-            var devices = await context.ListDevices();
-            var device = devices.Devices.First(x => x.Name.Contains(Config.DevicePartialName));
-
-            Console.WriteLine(device.Name);
-
-            await context.Initialize(new InitializeRequest
-            {
-                DeviceId = devices.Devices[1].Id,
-                EncryptionKey = Config.EncryptionKey
-            });
-
-            var amount = 1;
-
-            var result = await makePayment(context, amount);
-
-            Console.WriteLine(result.Result.CardHash);
-        }
-
-        private static async Task<ProcessPaymentResponse> makePayment(Context context, int amount)
-        {
-            var result = await context.ProcessPayment(new ProcessPaymentRequest
-            {
-                Amount = amount
-            });
-
-            var transaction = new Transaction
-            {
-                CardHash = result.Result.CardHash,
-                Amount = amount,
-                ShouldCapture = false
-            };
-
-            await transaction.SaveAsync();
-
-            await context.FinishPayment(new FinishPaymentRequest
-            {
-                Success = true,
-                ResponseCode = Int32.Parse(transaction.AcquirerResponseCode),
-                EmvData = transaction.CardEmvResponse,
-            });
-
-            return result;
-        }
     }
 }
