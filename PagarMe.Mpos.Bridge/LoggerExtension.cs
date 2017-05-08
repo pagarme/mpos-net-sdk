e42f1a2953096f5a1fb34dd86cdb8da7d79ae864
Lucas Duarte <lc.sales.duarte@gmail.com>
2017-05-08 19:18:36 -0300

    Make more logs and set path for WebSocket-Sharp logs

---
+++ PagarMe.Mpos.Bridge/LoggerExtension.cs
@@ -0,0 +1,52 @@
+﻿using NLog;
+using System;
+using System.Threading.Tasks;
+
+namespace PagarMe.Mpos.Bridge
+{
+    public static class LoggerExtension
+    {
+        public static void TryLogOnException(this Logger logger, Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception e)
+            {
+                try
+                {
+                    logger.Error(e);
+                }
+                catch
+                {
+                    // Log error should not override other errors
+                }
+
+                throw;
+            }
+        }
+
+        public static async Task TryLogOnExceptionAsync(this Logger logger, Func<Task> action)
+        {
+            try
+            {
+                await action();
+            }
+            catch (Exception e)
+            {
+                try
+                {
+                    logger.Error(e);
+                }
+                catch
+                {
+                    // Log error should not override other errors
+                }
+
+                throw;
+            }
+        }
+
+    }
+}
