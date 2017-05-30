fccb3c251e64b2bdefa77f47f343663b43e13107
Lucas Duarte <lc.sales.duarte@gmail.com>
2017-05-30 16:57:30 -0300

    chore: pattern - no semi-collon

--- docs/example-site/config.js
+++ docs/example-site/config.js
@@ -1,141 +1,141 @@
-var wsWrap = null;
-var devices = null;
+var wsWrap = null
+var devices = null
 
 function init () {
-  setIfNull('baud-rate');
+  setIfNull('baud-rate')
 
-  const contextId = getById('context-id').value;
-  wsWrap = new WebSocketWrap(contextId);
+  const contextId = getById('context-id').value
+  wsWrap = new WebSocketWrap(contextId)
 
-  getPortList();
+  getPortList()
 }
 
 function setIfNull (name) {
-  const currentValue = getById(name).value;
+  const currentValue = getById(name).value
 
   if (!currentValue) {
-    getById(name).value = getLocal(name);
+    getById(name).value = getLocal(name)
   }
 }
 
 function getPortList () {
-  wsWrap.call(wsWrap.listDevices, handleResponse);
+  wsWrap.call(wsWrap.listDevices, handleResponse)
 }
 
 function handleResponse (response) {
-  const responseJson = JSON.parse(response.data);
+  const responseJson = JSON.parse(response.data)
 
   switch (responseJson.response_type) {
     case wsWrap.response.devicesListed:
-      populateDeviceList(responseJson);
-      break;
+      populateDeviceList(responseJson)
+      break
 
     case wsWrap.response.alreadyInitialized:
     case wsWrap.response.initialized:
-      wsWrap.displayMessage('Configurado');
-      break;
+      wsWrap.displayMessage('Configurado')
+      break
 
     case wsWrap.response.messageDisplayed:
-      configured();
-      break;
+      configured()
+      break
 
     case wsWrap.response.closed:
-      break;
+      break
 
     default:
-      wsWrap.ws.close();
+      wsWrap.ws.close()
 
-      const message = getEndingMessage(wsWrap, responseJson);
-      if (message) showMessage(message);
+      const message = getEndingMessage(wsWrap, responseJson)
+      if (message) showMessage(message)
 
-      break;
+      break
   }
-};
+}
 
 function populateDeviceList (responseJson) {
-  devices = responseJson.device_list;
-  const devicePortSelect = getById('device-port');
+  devices = responseJson.device_list
+  const devicePortSelect = getById('device-port')
 
   if (devices.length === 0) {
-    devicePortSelect.innerHTML = '<option value="">---</option>';
-    showMessage('Não foram encontradas portas');
-    toggleSaveButton(false);
-    return;
+    devicePortSelect.innerHTML = '<option value="">---</option>'
+    showMessage('Não foram encontradas portas')
+    toggleSaveButton(false)
+    return
   }
 
-  devicePortSelect.innerHTML = '<option value="">-- Selecione --</option>';
-  const chosenPort = getLocal('device-port');
+  devicePortSelect.innerHTML = '<option value="">-- Selecione --</option>'
+  const chosenPort = getLocal('device-port')
 
   for(let d = 0; d < devices.length; d++) {
-    const selected = chosenPort === devices[d].port ? 'selected' : '';
+    const selected = chosenPort === devices[d].port ? 'selected' : ''
 
     devicePortSelect.innerHTML +=
       '<option value="' + devices[d].port + '"'
         + selected +
       '>'
         + devices[d].port +
-      '</option>';
+      '</option>'
   }
 
-  toggleSaveButton(true);
+  toggleSaveButton(true)
 }
 
 function toggleSaveButton (enabled) {
-  const button = getById('save');
-  button.className = 'btn btn-' + (enabled ? 'primary' : 'mute');
-  button.disabled = !enabled;
+  const button = getById('save')
+  button.className = 'btn btn-' + (enabled ? 'primary' : 'mute')
+  button.disabled = !enabled
 }
 
 function configured () {
-  setLocal('device-port', getSelected(getById('device-port')));
-  setLocal('baud-rate', getById('baud-rate').value);
-  showMessage('Configurações salvas no navegador');
+  setLocal('device-port', getSelected(getById('device-port')))
+  setLocal('baud-rate', getById('baud-rate').value)
+  showMessage('Configurações salvas no navegador')
 }
 
 function getEndingMessage (wsWrap, responseJson) {
   switch (responseJson.response_type) {
     case wsWrap.response.error:
-      return responseJson.error;
+      return responseJson.error
 
     case wsWrap.response.unknownCommand:
-      return 'Comando desconhecido';
+      return 'Comando desconhecido'
 
     default:
-      return 'Resposta desconhecida';
+      return 'Resposta desconhecida'
   }
-};
+}
 
 function testAndSaveConfig () {
-  const encryptionKey = '';
+  const encryptionKey = ''
 
   if (!devices) {
-    showMessage('Carregue as opções primeiro');
-    return;
+    showMessage('Carregue as opções primeiro')
+    return
   }
 
-  const devicePort = getSelected(getById('device-port'));
+  const devicePort = getSelected(getById('device-port'))
 
   if (!devicePort) {
-    showMessage('Porta inválida');
-    return;
+    showMessage('Porta inválida')
+    return
   }
 
-  const baudRate = getById('baud-rate').value;
+  const baudRate = getById('baud-rate').value
 
   if (!baudRate) {
-    showMessage('Taxa de transmissão inválida');
-    return;
+    showMessage('Taxa de transmissão inválida')
+    return
   }
 
   for(let d = 0; d < devices.length; d++) {
     if (devices[d].port === devicePort) {
-      wsWrap.initialize('', devices[d].id, baudRate, true);
+      wsWrap.initialize('', devices[d].id, baudRate, true)
     }
   }
 
   if (!devices) {
-    showMessage('Porta ' + devicePort + ' não encontrada');
+    showMessage('Porta ' + devicePort + ' não encontrada')
   }
 }
 
-init();
+init()
