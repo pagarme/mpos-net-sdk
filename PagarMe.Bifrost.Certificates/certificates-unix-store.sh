113f4e97bd2860e3a3cad27cdcf2b0cb157cfdeb
Lucas Duarte <lc.sales.duarte@gmail.com>
2017-06-06 13:23:58 -0300

    chore: change certificate name because of identity

--- PagarMe.Bifrost.Certificates/certificates-unix-store.sh
+++ PagarMe.Bifrost.Certificates/certificates-unix-store.sh
@@ -3,9 +3,9 @@ apt-get update
 apt-get install libnss3-tools
 
 # Chrome
-certutil -d sql:$HOME/.pki/nssdb -D -n MPOS 2> /dev/null
-certutil -d sql:$HOME/.pki/nssdb -A -t "P,," -n MPOS -i $1/$2.crt
+certutil -d sql:$HOME/.pki/nssdb -D -n Bifrost 2> /dev/null
+certutil -d sql:$HOME/.pki/nssdb -A -t "P,," -n Bifrost -i $1/$2.crt
 
 # Firefox
-certutil -d $(find ~/.mozilla/ -name cert*.db | sed s/cert[0-9].db//g | sort | uniq) -D -n MPOS 2> /dev/null
-certutil -d $(find ~/.mozilla/ -name cert*.db | sed s/cert[0-9].db//g | sort | uniq) -A -t "TCu,Cuw,Tuw" -n MPOS -i $1/$2.crt
+certutil -d $(find ~/.mozilla/ -name cert*.db | sed s/cert[0-9].db//g | sort | uniq) -D -n Bifrost 2> /dev/null
+certutil -d $(find ~/.mozilla/ -name cert*.db | sed s/cert[0-9].db//g | sort | uniq) -A -t "TCu,Cuw,Tuw" -n Bifrost -i $1/$2.crt
