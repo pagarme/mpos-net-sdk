0d32f428f8eae25eda2e9f9af45ac47dbaaa8227
Lucas Duarte <lc.sales.duarte@gmail.com>
2017-06-12 16:43:03 -0300

    Create Generic Assembly Info
    Use one assembly info for all Mpos projects, to put all
    common information inside one file.

--- PagarMe.Generic/Properties/AssemblyInfo.cs
+++ PagarMe.Generic/Properties/AssemblyInfo.cs
@@ -1,17 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTitle("PagarMe.Generic")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PagarMe.Generic")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -22,15 +18,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0b590c92-a6fb-4af2-a275-05042eb8f4a1")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
