35637fdbce138193b2bbae3ad1c6d1d15adc13f0
Lucas Duarte <lc.sales.duarte@gmail.com>
2017-06-12 16:43:03 -0300

    Create Bifrost Generic Assembly Info
    Use one assembly info for all Bifrost/Mpos projects, to put all
    common information inside one file.

---
+++ PagarMe.Bifrost/Properties/BifrostAssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Pagarme.Bifrost")]
+[assembly: AssemblyDescription("Pagar.me Bifrost")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Pagar.me mPOS")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
