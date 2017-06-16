5f00c218022662dcdeb34f2ad2480736bab02cc5
Lucas Duarte <lc.sales.duarte@gmail.com>
2017-06-16 15:55:35 -0300

    Create auto-updater for windows of service
    When starts, the service will check if it's on the right version (can
    be used to downgrade too). If it isn't, will download the new msi,
    uninstall the old version based on the product code found at the
    downloaded msi, which stops the service, and install the new version,
    which restarts the service.

---
+++ PagarMe.Bifrost/Updates/Product.cs
@@ -0,0 +1,33 @@
+﻿using NLog;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace PagarMe.Bifrost.Updates
+{
+    public class Product
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static String GetCode(String filename)
+        {
+            try
+            {
+                var content = File.ReadAllText(filename);
+
+                var guidFormat = "{[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}}";
+                var pattern = $@"ProductName({guidFormat})";
+                var regex = new Regex(pattern);
+                var matches = regex.Matches(content);
+
+                return matches[0].Groups[1].Value;
+            }
+            catch (Exception e)
+            {
+                logger.Error("Program registry may be not found:");
+                logger.Error(e);
+                return null;
+            }
+        }
+    }
+}
