2e3dfa6228bfd1f2e4a2b2d254d0c220de3ce41b
Lucas Duarte <lc.sales.duarte@gmail.com>
2017-06-16 15:55:35 -0300

    Create auto-updater for windows of service
    When starts, the service will check if it's on the right version (can
    be used to downgrade too). If it isn't, will download the new msi,
    uninstall the old version based on the product code found at the
    downloaded msi, which stops the service, and install the new version,
    which restarts the service.

---
+++ PagarMe.Bifrost/Updates/Request.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using NLog;
+using PagarMe.Bifrost.WebSocket;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace PagarMe.Bifrost.Updates
+{
+    public class RequestMaker
+    {
+        public RequestMaker(String mainUrl)
+        {
+            this.mainUrl = mainUrl;
+        }
+
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        private readonly string mainUrl;
+
+        public async Task<T> GetObjectFromUrl<T>(String path = null)
+        {
+            var json = await getContentFromUrl(path);
+            return JsonConvert.DeserializeObject<T>(json, SnakeCase.Settings);
+        }
+
+        private async Task<String> getContentFromUrl(String path = null)
+        {
+            return await getFromUrl((stream) =>
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }, path);
+        }
+
+        private async Task<T> getFromUrl<T>(Func<Stream, T> action, String path = null)
+        {
+            var request = WebRequest.Create($"{mainUrl}/{path}");
+
+            var response = (HttpWebResponse)await request.GetResponseAsync();
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                logger.Warn($"Update failed: {mainUrl}/{path} returned status code {response.StatusCode}");
+                return default(T);
+            }
+
+            using (var stream = response.GetResponseStream())
+            {
+                return action(stream);
+            }
+        }
+
+        public async Task<Boolean> DownloadBinaryFromUrl(String filename, String path = null)
+        {
+            return await Task.Run(() =>
+            {
+                try
+                {
+                    var client = new WebClient();
+                    client.DownloadFile($"{mainUrl}/{path}", filename);
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    logger.Error(e);
+                    return false;
+                }
+            });
+        }
+    }
+}
---
+++ PagarMe.Bifrost/Updates/WindowsUpdater.cs
@@ -0,0 +1,98 @@
+﻿using NLog;
+using PagarMe.Generic;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace PagarMe.Bifrost.Updates
+{
+    public class WindowsUpdater
+    {
+        // Temporary address, will be defined when distribution is decided
+        private const String Address = "http://localhost:2001";
+
+        private static RequestMaker request = new RequestMaker(Address);
+        private static Version currentVersion = Assembly.GetEntryAssembly().GetName().Version;
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public async static Task<Boolean> CheckAndUpdate()
+        {
+            logger.Info("Update check starting");
+
+            var updated = false;
+
+            try
+            {
+                updated = await checkAndUpdate();
+            }
+            catch (Exception e)
+            {
+                logger.Error(e);
+            }
+
+            return updated;
+        }
+
+        private async static Task<Boolean> checkAndUpdate()
+        {
+            var update = await request.GetObjectFromUrl<Update>();
+            var shouldUpgradeOrDowngrade = currentVersion != update.LastVersion;
+
+            if (!shouldUpgradeOrDowngrade)
+            {
+                logger.Info($"Bifrost up to date: {currentVersion}");
+                return false;
+            }
+
+            var filename = $"bifrost-installer-{update.LastVersion}.msi";
+            var filepath = Path.Combine(logger.GetLogDirectoryPath(), filename);
+            var downloaded = await request.DownloadBinaryFromUrl(filepath, filename);
+
+            if (!downloaded) return false;
+
+            return await Task.Run(() =>
+            {
+                return reinstall(filepath, update.LastVersion);
+            });
+        }
+
+        private static Boolean reinstall(String filepath, Version newVersion)
+        {
+            var code = Product.GetCode(filepath);
+            if (code == null) return false;
+
+            var succededUninstall = runOnMsi($"/x {code}", "uninstall");
+            if (!succededUninstall) return false;
+
+            var succededInstall = runOnMsi($"/i {filepath}", "install");
+            if (!succededInstall) return false;
+
+            logger.Info($"Finished upgrading.");
+            logger.Info($"New version: {newVersion}.");
+
+            return true;
+        }
+
+        private static bool runOnMsi(String command, String description)
+        {
+            logger.Info($"Starting {description}");
+            var result = Terminal.Run("msiexec", command, "/quiet");
+
+            if (!result.Succedded)
+            {
+                logger.Warn(result.Output);
+                logger.Warn(result.Error);
+                logger.Warn($"Error: {description} process exited with code {result.Code}");
+            }
+
+            return result.Succedded;
+        }
+
+        class Update
+        {
+            public String LastVersionName { get; set; }
+            public Version LastVersion => new Version(LastVersionName);
+        }
+    }
+}
