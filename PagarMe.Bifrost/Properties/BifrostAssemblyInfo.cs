286da5123f79d60363565d7dd646f86551df95d3
Lucas Duarte <lc.sales.duarte@gmail.com>
2017-06-27 15:36:22 -0300

    chore: version
    # Conflicts:
    #	PagarMe.Bifrost.Setup/PagarMe.Bifrost.Setup.vdproj
    #	PagarMe.Bifrost/Properties/BifrostAssemblyInfo.cs

--- PagarMe.Bifrost/Properties/BifrostAssemblyInfo.cs
+++ PagarMe.Bifrost/Properties/BifrostAssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.0.8")]
-[assembly: AssemblyFileVersion("0.0.8")]
+[assembly: AssemblyVersion("0.0.14")]
+[assembly: AssemblyFileVersion("0.0.14")]
