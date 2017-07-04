887c44196badc9160baa5f003ab10c68ca0e4ffd
Lucas Duarte <lc.sales.duarte@gmail.com>
2017-07-04 20:50:17 -0300

    Create delete if exists
    Create DeleteIfExists extra method for files

---
+++ PagarMe.Generic/FileExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace PagarMe.Generic
+{
+    public class FileExtension
+    {
+        public static void DeleteIfExists(String path)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+    }
+}
