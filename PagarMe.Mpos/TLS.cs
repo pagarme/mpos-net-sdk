4a6b36a1cd13bdbc76370badd3fcaee8c4b0cd79
Dara Keon <lc.sales.duarte@gmail.com>
2018-06-20 13:23:55 -0300

    tls: fix api communication
    The api, after 2018-06-21, will only accept communication using
    TLS 1.2, for security reasons.

---
+++ PagarMe.Mpos/TLS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace PagarMe.Mpos
+{
+    public class TLS
+    {
+        /// <summary>
+        /// Changes the default protocol to TLS 1.2
+        /// to run the request that needs this protocol
+        /// then return to previous configuration
+        /// </summary>
+        public static async Task<T> RunWithTLS12<T>(Func<Task<T>> requestCode)
+        {
+            var defaultProtocol = ServicePointManager.SecurityProtocol;
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            var result = await requestCode();
+
+            ServicePointManager.SecurityProtocol = defaultProtocol;
+
+            return result;
+        }
+    }
+}
