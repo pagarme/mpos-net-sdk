eb84aca16fa1a1bdc18fb9fb53c6d0d50f5208a0
Dara Keon <lc.sales.duarte@gmail.com>
2019-04-17 21:21:34 -0300

    log: add centralized method

--- PagarMe.Mpos.Example/Program.cs
+++ PagarMe.Mpos.Example/Program.cs
@@ -16,20 +16,11 @@ namespace PagarMe.Mpos.Example
         {
             var processor = new PaymentProcessor(Config.Port);
 
-            Console.WriteLine("Welcome to Pagador 9000");
-            Console.WriteLine("Initializing...");
-
-
+            PgDebugLog.Write("Will initialize");
             await processor.Initialize();
+            PgDebugLog.Write("Initialized");
 
-            //System.Threading.Thread.Sleep (3000);
-            Console.Write("Amount: ");
-            //String x = Console.ReadLine ();
-            //Console.WriteLine ("read line " + x);
-            //Int32 integer = Int32.Parse (x);
             await processor.Pay(100);
-
-            // Console.WriteLine("Created transaction {0}.", transaction.Id);
         }
     }
 }
